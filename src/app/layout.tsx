68e33d781ea14d0a94b82ca0c1a0cb7929db7fd0
Sergey.M <sergey.m@marakas.digital>
2024-02-21 15:29:26 +0200

    add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "Unilime SpeedUp: Shopify Pagespeed App",
     description: "We increase the conversion rate by increasing Core Web Vitals metrics\n",
+    verification: {
+        google: "k5N-HIiMH2y6swBG7i4aYnmzZKQnyChjoRY3gGtWt3w",
+    },
 };
 
 export default function RootLayout({
