19e421f98cdb3d6d1ddbd502eefe5b18bc2d5441
Antonina Shabala <shabala.antonina@gmail.com>
2024-04-15 19:04:32 +0300

    changelog page

--- src/app/components/common/Footer/index.tsx
+++ src/app/components/common/Footer/index.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
                     </p>
                 </a>
                 <a
-                    href="/faq"
+                    href="/changelog"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     rel="noopener noreferrer"
                 >
