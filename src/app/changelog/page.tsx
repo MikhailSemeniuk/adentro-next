012c23fd70eb72403e63db6c5c38ef61b03a8ccb
Antonina Shabala <shabala.antonina@gmail.com>
2024-04-16 10:37:34 +0300

    changelog page

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -22,30 +22,51 @@ export default function Changelog() {
                         <p className="mmb-4 text-lg font-normal text-gray-500 dark:text-gray-400">
                             Read more about the releases made for SpeedUp from our changelog
                         </p>
-                        <div className="flex w-full  py-8">
-                            <div className="flex-auto max-w-4xl min-w-0 pt-6 lg:pt-8">
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                        <div className="flex w-full  pt-8">
+                            <div className="flex-auto pt-6 lg:pt-8">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.6
                                     <span id="changelog-1-0-6" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-6" aria-label="Link to this section: changelog-1-0-6">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
                                     <li>Minor changes to configurations</li>
                                 </ul>
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.5
                                     <span id="changelog-1-0-5" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-5" aria-label="Link to this section: changelog-1-0-5">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
                                     <li>Updated configuration to add privacy compliance webhooks</li>
                                 </ul>
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.4
                                     <span id="changelog-1-0-4" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-4" aria-label="Link to this section: changelog-1-0-4">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
@@ -53,20 +74,34 @@ export default function Changelog() {
                                     <li>Fixed minor bugs</li>
                                     <li>Implemented mandatory compliance webhooks</li>
                                 </ul>
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.3
                                     <span id="changelog-1-0-3" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-3" aria-label="Link to this section: changelog-1-0-3">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
                                     <li>Hotfixes</li>
                                     <li>Improved PageSpeed optimization script</li>
                                 </ul>
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.2
                                     <span id="changelog-1-0-2" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-2" aria-label="Link to this section: changelog-1-0-2">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
@@ -75,10 +110,17 @@ export default function Changelog() {
                                     <li>Support Page</li>
                                     <li>Hotfixes</li>
                                 </ul>
-                                <h2 className="relative group mb-4 text-4xl font-bold dark:text-white">
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
                                     1.0.1
                                     <span id="changelog-1-0-1" className="absolute -top-[40px]"></span>
-                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100"
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
                                        href="#changelog-1-0-1" aria-label="Link to this section: changelog-1-0-1">#</a>
                                 </h2>
                                 <ul className={`space-y-1 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
