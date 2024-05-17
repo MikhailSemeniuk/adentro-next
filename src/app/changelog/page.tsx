955632038acf4929edf86da182d82d118c852578
Victoria Bogutska <victoria.bogutska@marakas.digital>
2024-05-17 14:14:35 +0300

    Add to changelog version 1.0.8

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -32,10 +32,28 @@ export default function Changelog() {
                                               stroke-width="2"
                                               d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
                                     </svg>
-                                    1.0.7
-                                    <span id="v-1-0-6" className="absolute -top-[40px]"></span>
+                                    1.0.8
+                                    <span id="v-1-0-8" className="absolute -top-[40px]"></span>
                                     <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
-                                       href="#v-1-0-6" aria-label="Link to this section: v-1-0-6">#</a>
+                                       href="#v-1-0-8" aria-label="Link to this section: v-1-0-8">#</a>
+                                </h2>
+                                <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
+                                    <li>Adding discount functionality</li>
+                                    <li>Fixed speedup script</li>
+                                    <li>Fixed welcome screen bug</li>
+                                </ul>
+                                <h2 className="relative flex items-center group mb-4 text-3xl font-bold dark:text-white">
+                                    <svg className="w-6 h-6 mr-2 text-gray-800 dark:text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                                    </svg>
+                                    1.0.7
+                                    <span id="v-1-0-7" className="absolute -top-[40px]"></span>
+                                    <a className="ml-2 text-blue-700 opacity-0 transition-opacity dark:text-blue-500 group-hover:opacity-100 hidden xl:block"
+                                       href="#v-1-0-7" aria-label="Link to this section: v-1-0-7">#</a>
                                 </h2>
                                 <ul className={`space-y-1 mb-9 text-lg text-gray-500 list-disc list-inside dark:text-gray-400`}>
                                     <li>Fixes for infinite loop on test payments</li>
